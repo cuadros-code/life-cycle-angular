a1fc0740de6c7489d38fa64192478e50b371e5f5
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-02-10 10:16:30 -0500

    initial commit

---
+++ src/app/components/show-name/show-name.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-name',
+  templateUrl: './show-name.component.html',
+  styleUrls: ['./show-name.component.css']
+})
+export class ShowNameComponent {
+
+}
